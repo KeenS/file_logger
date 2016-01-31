2dd1131c764e8a512334e960bdc75f56bedbec4a
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2016-01-31 14:32:18 +0900

    generate docs

---
+++ file_logger/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["OnError",""]],"struct":[["Logger",""],["LoggerBuilder",""]]});
---
+++ implementors/log/trait.Log.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['file_logger'] = ["impl&lt;W: <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a>&gt; <a class='trait' href='log/trait.Log.html' title='log::Log'>Log</a> for <a class='struct' href='file_logger/struct.Logger.html' title='file_logger::Logger'>Logger</a>&lt;W&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
